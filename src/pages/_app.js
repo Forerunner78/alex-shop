b43ab2a2865e37f8b06ba5406e87acdf6c9dc490
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-24 14:43:51 +0200

    Creation of the Header, Footer and Layout components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,8 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Layout from "@/components/Layout";
 import { Roboto } from "next/font/google";
+import Head from "next/head";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -6,7 +10,18 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<main className={roboto.className}>
-				<Component {...pageProps} />;
+				<Head>
+					<title>Alex&apos;s Shop</title>
+					<meta name="description" content="Ecommerce website built with MERN" />
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<Header />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+
+				<Footer />
 			</main>
 		</>
 	);
