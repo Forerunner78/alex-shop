a085eecbb1a770daf9d984ba5cdd642c1c76decc
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-25 15:54:28 +0200

    Build the connection with mongoDB

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,16 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+	users: [
+		{
+			name: "Admin User",
+			email: "admin@example.com",
+			password: bcrypt.hashSync("123456", 10),
+			isAdmin: true,
+		},
+		{ name: "John Doe", email: "john@example.com", password: bcrypt.hashSync("123456", 10) },
+		{ name: "Jane Doe", email: "jane@example.com", password: bcrypt.hashSync("123456", 10) },
+	],
 	products: [
 		{
 			id: 1,
