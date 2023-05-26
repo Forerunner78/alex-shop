4a879d95c7443353e9588e293eaaf24ff7f5b2ab
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-26 15:10:18 +0200

    Creation of login API

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import Image from "next/image";
 import Layout from "./Layout";
 import { useContext, useEffect, useState } from "react";
 import { Store } from "@/utils/Store";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+	const { status, data: session } = useSession();
 	const { state } = useContext(Store);
 	const { cart } = state;
 	const [cartItemsCount, setCartItemsCount] = useState(0);
@@ -34,7 +36,14 @@ const Header = () => {
 					<Link href="/cart">
 						Cart {cartItemsCount > 0 && <span>{cartItemsCount}</span>}
 					</Link>
-					<Link href="/login">Login</Link>
+
+					{status === "loading" ? (
+						"Loading"
+					) : session?.user ? (
+						"Welcome" + " " + session.user.name + "!"
+					) : (
+						<Link href="/login">Login</Link>
+					)}
 				</div>
 			</Layout>
 		</header>
