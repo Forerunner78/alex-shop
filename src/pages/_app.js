4ae98e67c7a1dde12c6424354b802f1b77118edd
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-26 15:11:46 +0200

    Creation of login API

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,15 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Layout from "@/components/Layout";
 import { StoreProvider } from "@/utils/Store";
+import { SessionProvider } from "next-auth/react";
 import { Roboto } from "next/font/google";
 import Head from "next/head";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<>
 			<main className={roboto.className}>
@@ -17,14 +20,17 @@ export default function App({ Component, pageProps }) {
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link rel="icon" href="/favicon.ico" />
 				</Head>
-				<StoreProvider>
-					<Header />
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
+				<SessionProvider session={session}>
+					<StoreProvider>
+						<ToastContainer limit={1} />
+						<Header />
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
 
-					<Footer />
-				</StoreProvider>
+						<Footer />
+					</StoreProvider>
+				</SessionProvider>
 			</main>
 		</>
 	);
