ae874081b2ba2276dce802ae80823a4f4986d156
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-30 11:37:07 +0200

    Creation of the payment screen

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -43,6 +43,14 @@ function reducer(state, action) {
 					shippingAddress: { ...state.cart.shippingAddress, ...action.payload },
 				},
 			};
+		case "SAVE_PAYMENT_METHOD":
+			return {
+				...state,
+				cart: {
+					...state.cart,
+					paymentMethod: action.payload,
+				},
+			};
 		default:
 			return state;
 	}
