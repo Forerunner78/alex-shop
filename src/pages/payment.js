03efcc23e6ab3fc18987efd4e58f099379599f19
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-30 11:37:07 +0200

    Creation of the payment screen

---
+++ src/pages/payment.js
@@ -0,0 +1,60 @@
+import CheckoutWizard from "@/components/CheckoutWizard";
+import { Store } from "@/utils/Store";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const PaymentScreen = () => {
+	const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
+	const { state, dispatch } = useContext(Store);
+	const { cart } = state;
+	const { shippingAddress, paymentMethod } = cart;
+	const router = useRouter();
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		if (!selectedPaymentMethod) {
+			return toast.error("Payment method is required");
+		}
+		dispatch({ type: "SAVE_PAYMENT_METHOD", payload: selectedPaymentMethod });
+		Cookies.set("cart", JSON.stringify({ ...cart, paymentMethod: selectedPaymentMethod }));
+		router.push("/placeorder");
+	};
+
+	useEffect(() => {
+		if (!shippingAddress.address) {
+			return router.push("/shipping");
+		}
+		setSelectedPaymentMethod(paymentMethod || "");
+	}, [paymentMethod, router, shippingAddress.address]);
+
+	return (
+		<>
+			<CheckoutWizard activeStep={2} />
+			<form onSubmit={submitHandler}>
+				<h1>Payment Method</h1>
+				{["Paypal", "Stripe", "CashOnDelivery"].map((payment) => (
+					<div key={payment}>
+						<input
+							name="paymentMethod"
+							id={payment}
+							type="radio"
+							checked={selectedPaymentMethod === payment}
+							onChange={() => setSelectedPaymentMethod(payment)}
+						/>
+						<label htmlFor={payment}>{payment}</label>
+					</div>
+				))}
+				<div>
+					<button onClick={() => router.push("/shipping")} type="button">
+						Back
+					</button>
+					<button>Next</button>
+				</div>
+			</form>
+		</>
+	);
+};
+
+export default PaymentScreen;
