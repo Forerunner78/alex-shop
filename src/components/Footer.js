9fa92c8c47f48bb67fd1b8211224e40f8d73d98a
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-06-06 09:26:21 +0200

    Footer contribution for small devices

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
-import Layout from "./Layout";
+import Image from "next/image";
+import Paypal from "./../../public/img/Paypal.png";
+import Visa from "./../../public/img/Visa.png";
 
 const Footer = () => {
 	return (
-		<footer>
-			<Layout>
-				<div>Copyright &copy; {new Date().getFullYear()} - Alex&apos;s Shop</div>
-			</Layout>
+		<footer className="mt-10 px-5 flex flex-row justify-between text-center">
+			<div className="flex flex-col items-center justify-center">
+				<h1 className="uppercase text-xl">Alex&apos;s Shop</h1>
+				<span className="text-xs">Copyright &copy; {new Date().getFullYear()}</span>
+			</div>
+			<div className="flex flex-row flex-end items-center">
+				<span>
+					<Image src={Paypal} width={100} alt="Paypal logo" />
+				</span>
+				<span>
+					<Image src={Visa} width={50} alt="Visa logo" />
+				</span>
+			</div>
 		</footer>
 	);
 };
