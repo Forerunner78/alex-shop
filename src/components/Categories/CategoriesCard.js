0fb3dc6d8b0e1a53a62bdc8691c41a46b1b6a768
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-06-13 17:16:02 +0200

    Add CSS to categories grid to make it responsive

--- src/components/Categories/CategoriesCard.js
+++ src/components/Categories/CategoriesCard.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const CategoriesCard = ({ categoryText, image, height }) => {
+const CategoriesCard = ({ categoryText, image }) => {
 	return (
-		<>
+		<div className="h-[25vh] xs:h-[30vh] sm:h-[40vh] md:h-[50vh] overflow-hidden">
 			<Link href="/">
-				<Image src={image} alt={categoryText} height={height}></Image>
+				<Image src={image} alt={categoryText} className="object-cover" fill></Image>
 			</Link>
-			<span className="absolute bg-slate-400/75 text-[65%]">{categoryText}</span>
-		</>
+			<span className="absolute top-[50%] -translate-y-[50%] -translate-x-[50%] left-[50%] bg-slate-400/75 p-1 text-[50%] sm:text-[75%] text-center lg:text-lg">
+				{categoryText}
+			</span>
+		</div>
 	);
 };
 
