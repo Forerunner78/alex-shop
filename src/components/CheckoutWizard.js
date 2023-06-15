0699c532c933eaf426aa8a8dcf3410e8668553df
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-06-15 18:36:48 +0200

    CSS changes for checkoutwizard

--- src/components/CheckoutWizard.js
+++ src/components/CheckoutWizard.js
@@ -3,25 +3,25 @@ const CheckoutWizard = ({ activeStep = 0 }) => {
 	const progressBar = progressValue[activeStep];
 
 	return (
-		<>
-			<div className="flex flex-row px-5 text-xs text-center mb-2">
+		<div className="relative -bottom-[15vh] w-[100vw]">
+			<div className="flex flex-row justify-between px-5 mb-2">
 				{["User Login", "Shipping Address", "Payment Method", "Place Order"].map(
 					(step, index) => (
 						<div
 							key={step}
 							className={`${
 								index == activeStep ? "text-black font-bold" : "text-gray-400"
-							}`}
+							} text-md sm:text-lg text-center px-2`}
 						>
 							{step}
 						</div>
 					)
 				)}
 			</div>
-			<div className="mb-6 h-1 w-full bg-neutral-200 dark:bg-neutral-600">
+			<div className="mb-6 h-1 w-full bg-neutral-200">
 				<div className="h-1 bg-black" style={{ width: progressBar }}></div>
 			</div>
-		</>
+		</div>
 	);
 };
 
