596c668d02dff3d2168ecb459d7861d23e581228
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-06-30 15:55:31 +0200

    Last CSS fix to correctly display filters

--- src/components/ProductNotFound.js
+++ src/components/ProductNotFound.js
@@ -2,8 +2,8 @@ import Layout from "./Layout";
 
 const ProductNotFound = () => {
 	return (
-		<Layout className="w-[vw] mx-10 pb-5">
-			<span className="text-lg">Sorry, no product found!</span>{" "}
+		<Layout className="mx-10">
+			<span className="text-lg lg:text-4xl">Sorry, no product found!</span>{" "}
 		</Layout>
 	);
 };
